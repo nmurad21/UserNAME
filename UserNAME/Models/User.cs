9a554f911d2cbd6a2c02665210b1abc86cb5e692
User <User@WIN-CUEGFOLCMBN>
2022-03-16 23:59:51 +0400

    Add project files.

---
+++ UserNAME/Models/User.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserNAME.Models
+{
+    class User
+    {
+        public string Username { get; set; }
+        private int _age;
+        private string pasvord;
+
+        public int Age
+        { 
+            get 
+            {
+                return _age;
+            } 
+            set 
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Yas menfi ola bilmez");
+                      _age = value;
+                }
+                else if (value>0)
+                {
+                    Console.WriteLine(value);
+                }
+                 _age = value;
+            } 
+        }
+        public string Password 
+        { 
+            get 
+            {
+                return pasvord;
+            } 
+            set 
+            {
+                if (value=="")
+                {
+                    Console.WriteLine("Password bos ola bilmez");
+                    pasvord =value;
+                }
+                else if (value.Length < 8)
+                {
+                    Console.WriteLine("Password 8 simvoldan kicik ola bilmez");
+                    pasvord = value;
+                }
+                else if (value.Length > 8)
+                {
+                    Console.WriteLine(value);
+                }
+                pasvord = value;
+            } 
+
+        }
+        public User(string username, int _age, string pasvord)
+        {
+            this.Username = username;
+            this.Password = pasvord;
+            this.Age = _age;
+        }
+    }
+}
---
+++ UserNAME/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UserNAME.Models;
+
+namespace UserNAME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User("Murad", 20, "1234");
+            Console.WriteLine(user1.Username, user1.Age, user1.Password);
+            
+
+        }
+    }
+}
